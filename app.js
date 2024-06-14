9d5d6174c9eebc8af8b271b56cd58f5cb6aa50cd
Eglknght <elang1089@gmail.com>
2024-06-14 20:34:41 +0700

    nodejs express

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('hello world')
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('<h2>Hello World!</h2>')
+})
+
+app.get('/about', (req, res) => {
+    res.send('<h1>Nama saya elang</h1>')
+})
+
+app.listen(port, () => {
+ console.log(`aplikasi sudah siap, buka http://localhost:${port}`)
+})
