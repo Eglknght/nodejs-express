685c7ad1d5603bb19784775ab7f3919fb1263c8c
Eglknght <elang1089@gmail.com>
2024-06-14 22:03:12 +0700

    routing dan parsing variabel ejs

--- app.js
+++ app.js
@@ -15,6 +15,18 @@ app.get('/about/:name', (req, res) => {
     res.render('about', {name: req.params.name})
 })
 
+app.get('/experience', (req, res) => {
+    let data_kandidat={
+        nama : "Eagle",
+        umur : 19,
+        pekerjaan : "Mahasiswa",
+        gaji : 8000000
+    }
+    res.render('exp', {
+        data_kandidat: data_kandidat
+    })
+})
+
 app.listen(port, () => {
  console.log(`aplikasi sudah siap, buka http://localhost:${port}`)
 })
